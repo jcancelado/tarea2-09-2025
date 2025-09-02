34f7937444a1b1b5c7dc68fd14e94fe245b5a669
jcancelado <jcancelado@unal.edu.co>
2025-09-02 15:35:11 -0500

    creacion de archivo y codigo base

---
+++ tarea.py
@@ -0,0 +1,13 @@
+def pedirnombre():
+    nombre = input("Ingresa tu nombre")
+def pedircarrera():
+    carrera = input("Ingresa tu carrera")
+def pediridea_de_proyecto():
+    idea_de_proyecto = input("Ingresa tu idea de proyecto")
+
+
+def main():
+    print("Hola, este programa solicitara datos acerca de tu proyecto")
+    print("Tu nombre es")
+    print("Tu carrera es")
+    print("Tu idea de proyecto es")
